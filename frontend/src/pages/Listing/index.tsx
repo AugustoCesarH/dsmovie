feb702d2efd17458e4215c65e70a5ce8de61ed70
Augusto Cesar <augustocesarhp541@gmail.com>
2022-03-11 04:05:17 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,35 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStars from "components/MovieStars/indes";
 import Pagination from "components/Navbar/Pagination";
 
 function Listing() {
-    return(
-      <Pagination />
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-lx-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 export default Listing;
